0d2e9d649daad4692ed654627753f63edcae9d81
Kritika Sah <sah.kritika@gmail.com>
2020-04-04 16:08:36 -0700

    created a Features component to render the main features of thermakube

---
+++ client/components/Features.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Features = () => {
+  return (
+    <div className='featuresContainer'>
+      <h2>Visualize Your Kubernetes Clusters</h2>
+      <h3>Why Use Us?</h3>
+    </div>
+  )
+}
+
+export default Features;
