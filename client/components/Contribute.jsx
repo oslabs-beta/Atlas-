bf938855affc4c79afec8c190b4524150ff1937f
Kritika Sah <sah.kritika@gmail.com>
2020-04-04 16:10:06 -0700

    created a Contribute component

---
+++ client/components/Contribute.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contribute = () => {
+  return (
+    <div className='contributeContainer'>
+      <h3>ThermaKube is an Open-Source Project</h3> 
+      <p>Feel free to contribute!</p>
+    </div>
+  )
+}
+
+export default Contribute;
